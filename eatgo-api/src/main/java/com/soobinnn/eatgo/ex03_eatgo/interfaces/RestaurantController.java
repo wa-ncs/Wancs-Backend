0b79fa4590e410872d2175bcc078810e12a672a9
Soobinnn <isb9082@naver.com>
2020-01-25 01:13:37 +0900

    add code style

--- eatgo-api/src/main/java/com/soobinnn/eatgo/ex03_eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/soobinnn/eatgo/ex03_eatgo/interfaces/RestaurantController.java
@@ -1,35 +1,30 @@
 package com.soobinnn.eatgo.ex03_eatgo.interfaces;
 
 import com.soobinnn.eatgo.ex03_eatgo.application.RestaurantService;
-import com.soobinnn.eatgo.ex03_eatgo.domain.MenuItem;
-import com.soobinnn.eatgo.ex03_eatgo.domain.MenuItemRepository;
 import com.soobinnn.eatgo.ex03_eatgo.domain.Restaurant;
-import com.soobinnn.eatgo.ex03_eatgo.domain.RestaurantRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class RestaurantController {
 
-    @Autowired
-    private RestaurantService restaurantService;
+  @Autowired private RestaurantService restaurantService;
 
-    @GetMapping("/restaurants")
-    public List<Restaurant> list() {
-        // List<Restaurant> restaurants = restaurantRepository.findAll();
-        List<Restaurant> restaurants = restaurantService.getRestaurants();
+  @GetMapping("/restaurants")
+  public List<Restaurant> list() {
+    // List<Restaurant> restaurants = restaurantRepository.findAll();
+    List<Restaurant> restaurants = restaurantService.getRestaurants();
 
-        return restaurants;
-    }
+    return restaurants;
+  }
 
-    @GetMapping("/restaurants/{id}")
-    public Restaurant detail(@PathVariable("id") Long id) {
-        Restaurant restaurant = restaurantService.getRestaurant(id);
+  @GetMapping("/restaurants/{id}")
+  public Restaurant detail(@PathVariable("id") Long id) {
+    Restaurant restaurant = restaurantService.getRestaurant(id);
 
-        return restaurant;
-    }
+    return restaurant;
+  }
 }
--- eatgo-api/src/test/java/com/soobinnn/eatgo/ex03_eatgo/application/RestaurantServiceTest.java
+++ eatgo-api/src/test/java/com/soobinnn/eatgo/ex03_eatgo/application/RestaurantServiceTest.java
@@ -1,73 +1,72 @@
 package com.soobinnn.eatgo.ex03_eatgo.application;
 
-import com.soobinnn.eatgo.ex03_eatgo.domain.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import com.soobinnn.eatgo.ex03_eatgo.domain.MenuItem;
+import com.soobinnn.eatgo.ex03_eatgo.domain.MenuItemRepository;
+import com.soobinnn.eatgo.ex03_eatgo.domain.Restaurant;
+import com.soobinnn.eatgo.ex03_eatgo.domain.RestaurantRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.BDDMockito.given;
-
 public class RestaurantServiceTest {
 
-    private RestaurantService restaurantService;
+  private RestaurantService restaurantService;
 
-    @Mock
-    private RestaurantRepository restaurantRepository;
+  @Mock private RestaurantRepository restaurantRepository;
 
-    @Mock
-    private MenuItemRepository menuItemRepository;
+  @Mock private MenuItemRepository menuItemRepository;
 
-    // 모든 테스트를 실행하기 전에 Before가 실행됨.
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
+  // 모든 테스트를 실행하기 전에 Before가 실행됨.
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
 
-        mockRestaurantRespository();
-        mockMenuItemRepository();
+    mockRestaurantRespository();
+    mockMenuItemRepository();
 
-        restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
-    }
+    restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+  }
 
-    private void mockRestaurantRespository() {
-        List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
-        restaurants.add(restaurant);
+  private void mockRestaurantRespository() {
+    List<Restaurant> restaurants = new ArrayList<>();
+    Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+    restaurants.add(restaurant);
 
-        given(restaurantRepository.findAll()).willReturn(restaurants);
-        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
-    }
+    given(restaurantRepository.findAll()).willReturn(restaurants);
+    given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+  }
 
-    private void mockMenuItemRepository() {
-        List<MenuItem> menuItems = new ArrayList<>();
-        menuItems.add(new MenuItem("Kimchi"));
+  private void mockMenuItemRepository() {
+    List<MenuItem> menuItems = new ArrayList<>();
+    menuItems.add(new MenuItem("Kimchi"));
 
-        given(menuItemRepository.findAllByRestaurantId(1004L))
-                .willReturn(menuItems);
-    }
+    given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
+  }
 
-    @Test
-    public void getRestaurants() {
-        List<Restaurant> restaurants = restaurantService.getRestaurants();
+  @Test
+  public void getRestaurants() {
+    List<Restaurant> restaurants = restaurantService.getRestaurants();
 
-        Restaurant restaurant = restaurants.get(0);
+    Restaurant restaurant = restaurants.get(0);
 
-        assertThat(restaurant.getId(), is(1004L));
-    }
+    assertThat(restaurant.getId(), is(1004L));
+  }
 
-    @Test
-    public void getRestaurant() {
-        Restaurant restaurant = restaurantService.getRestaurant(1004L);
+  @Test
+  public void getRestaurant() {
+    Restaurant restaurant = restaurantService.getRestaurant(1004L);
 
-        assertThat(restaurant.getId(), is(1004L));
+    assertThat(restaurant.getId(), is(1004L));
 
-        MenuItem menuItem = restaurant.getMenuItems().get(0);
+    MenuItem menuItem = restaurant.getMenuItems().get(0);
 
-        assertThat(menuItem.getName(), is("Kimchi"));
-    }
+    assertThat(menuItem.getName(), is("Kimchi"));
+  }
 }
