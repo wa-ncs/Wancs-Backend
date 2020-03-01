bc9020ac64a653cb9e12e45383216c37f062865d
Bahng Ji Hoon <wpxm2003@naver.com>
2020-03-01 12:37:52 +0900

    post 작업중...

--- wancs-api/src/main/java/com/jihoon/wancs/interfaces/BoardController.java
+++ wancs-api/src/main/java/com/jihoon/wancs/interfaces/BoardController.java
@@ -5,6 +5,7 @@ import com.jihoon.wancs.domain.Board;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +16,23 @@ public class BoardController {
 
   final private BoardService boardService;
 
+  //순환 참조를 막기 위해 필드 인젝션이 아닌 생성자 인젝션 사용
   @Autowired
   public BoardController(BoardService boardService){
     this.boardService = boardService;
   }
 
   @GetMapping
-  public List<Board> getBoards(
+  public List<Board> getList(
       @RequestParam(value="categoryNo", required = false, defaultValue = "-1") int categoryNo
   ) {
     Board board = new Board();
     board.setCategoryNo(categoryNo);
     return boardService.getList(board);
+  }
+
+  @PostMapping
+  public void addBoard(){
+
   }
 }
--- wancs-api/src/test/java/com/jihoon/wancs/interfaces/BoardControllerTests.java
+++ wancs-api/src/test/java/com/jihoon/wancs/interfaces/BoardControllerTests.java
@@ -1,11 +1,54 @@
 package com.jihoon.wancs.interfaces;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.jihoon.wancs.application.BoardService;
+import com.jihoon.wancs.domain.Board;
+import java.util.ArrayList;
+import java.util.List;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder.BindingResolver.StreamWriting;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-class BoardControllerTests {
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(BoardController.class)
+public class BoardControllerTests {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @MockBean
+  private BoardService BoardService;
 
   @Test
-  void getBoards() {}
+  void getList() throws Exception {
+    List<Board> boards = new ArrayList<>();
+    Board board = Board.builder()
+        .boardNo(3L)
+        .title("what are you from2222")
+        .content("India")
+        .views(1)
+        .useYN('Y')
+        .categoryNo(1)
+        .userNo(1)
+        .build();
+    boards.add(board);
+
+    given(BoardService.getList(board)).willReturn(boards);
+
+    mvc.perform(get("/v1/board"))
+        .andExpect(status().isOk())
+        //.andExpect(content().string(containsString("\"boardNo\":3")))
+        //.andExpect(content().string(containsString("\"title\":\"what are you from2222\"")));
+        .andExpect(content().string(containsString("")));
+  }
 }
